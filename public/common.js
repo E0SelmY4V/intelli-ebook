d86abd6aee194fa23a7ce333589a575e36211e06
E0SelmY4V <yabo1887415157@qq.com>
2025-08-31 10:45:41 +0800

    add: 后台出问题也可以显示

--- public/common.js
+++ public/common.js
@@ -1,9 +1,9 @@
-/**@type {((error: Error) => never) | ((...texts: readonly string[]) => never)} */
-function wrong(...text) {
-	const error = text.length > 1 ? text.join('\n') : text[0].toString() ?? 'Unknown';
+/**@type {(error: Error | readonly string[], front: boolean) => never)} */
+function wrong(error, front = true) {
+	error = Array.isArray(error) ? error.join('\n') : error?.toString() ?? 'Unknown';
 	const div = document.createElement("div");
 	div.innerHTML = `
-		<h1>我的天啊页面出问题了！</h1>
+		<h1>我的天啊${front ? '页面' : '后台'}出问题了！</h1>
 		<br />
 		<hr color="#fff" />
 		<br />
@@ -17,12 +17,15 @@ function wrong(...text) {
 /**@type {(url: string, init: RequestInit) => Promise<Response>} */
 async function req(url, init) {
 	const r = await fetch(url, init);
-	if (!r.ok) wrong(
+	if (!r.ok) wrong([
 		`${r.status} ${r.statusText}`,
 		'',
 		Array.from(r.headers.entries()).map(([k, v]) => `${k}: ${v};`).join('\n'),
 		await r.text(),
-	);
+	]);
 	return r;
 }
 
+onload = () => {
+	document.getElementsByName("from_input").forEach(n => n.value = location);
+};
