c61ae534bb991921811724ef2b34bc352aa50c5f
E0SelmY4V <yabo1887415157@qq.com>
2025-08-31 10:09:15 +0800

    add: 简单获取章节

--- ebook/main.js
+++ ebook/main.js
@@ -1,6 +1,14 @@
-function hh() {
+/// <reference path="../public/common.js" />
+
+/**@type {() => number} */
+function getCid() {
 	const a = new URLSearchParams(window.location.search);
-	console.log(a.get("cid"))
+	return a.get("cid") ?? wrong(Error("不知道用户要看什么章节"));
 }
-hh();
+
+async function getBookNow() {
+	const cid = getCid();
+	const text = await (await req(`./book_${cid}`)).text();
+	view_div.innerHTML = text;
+}
 
--- public/common.js
+++ public/common.js
@@ -1,5 +1,6 @@
-/**@type {(error: Error) => never} */
-function wrong(error) {
+/**@type {((error: Error) => never) | ((...texts: readonly string[]) => never)} */
+function wrong(...text) {
+	const error = text.length > 1 ? text.join('\n') : text[0].toString() ?? 'Unknown';
 	const div = document.createElement("div");
 	div.innerHTML = `
 		<h1>我的天啊页面出问题了！</h1>
@@ -7,9 +8,21 @@ function wrong(error) {
 		<hr color="#fff" />
 		<br />
 		<p>请你带着以下错误报告向管理员汇报，或者试试刷新也可以。</p>
-		<pre id="wrong_explain_pre">${error.toString() || 'Unknown'}</pre>
+		<pre id="wrong_explain_pre">${error}</pre>
 	`;
 	div.id = 'wrong_div';
 	document.children[0].appendChild(div);
+	throw error;
+}
+/**@type {(url: string, init: RequestInit) => Promise<Response>} */
+async function req(url, init) {
+	const r = await fetch(url, init);
+	if (!r.ok) wrong(
+		`${r.status} ${r.statusText}`,
+		'',
+		Array.from(r.headers.entries()).map(([k, v]) => `${k}: ${v};`).join('\n'),
+		await r.text(),
+	);
+	return r;
 }
 
