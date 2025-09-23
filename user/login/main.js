ffe9bf2e1994cb7874bc2edcd5f4c524afab7492
E0SelmY4V <yabo1887415157@qq.com>
2025-09-23 22:46:06 +0800

    add: 提示忘记密码页面

--- user/login/main.js
+++ user/login/main.js
@@ -11,6 +11,6 @@ setOnload(new CallbackHandler({
 	no_id: ["login", ["没输账号", "请重输"]],
 	no_password: ["login", ['密码不能为空', '请重输']],
 	no_user: ["login", ['找不到此用户', '请检查你的邮箱或用户名是否正确']],
-	wrong_password: ["login", ['密码错误', '请重输']],
+	wrong_password: ["login", ['密码错误', '请重输<br /><br />忘记密码可通过<a href="/user/signup/">此处</a>重置']],
 }).handle);
 
