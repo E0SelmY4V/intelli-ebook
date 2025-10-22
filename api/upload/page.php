faf798093ab9338d9cd0ac43bf45130656a3ee2e
E0SelmY4V <yabo1887415157@qq.com>
2025-10-23 02:07:30 +0800

    add: 上传文件功能

--- api/upload/page.php
+++ api/upload/page.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../scpo-php/db.php';
-require_once __DIR__ . '/../scpo-php/errpage.php';
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/../../scpo-php/db.php';
+require_once __DIR__ . '/../../scpo-php/errpage.php';
+require_once __DIR__ . '/../config.php';
+require __DIR__ . '/dir.php';
 
 use \ScpoPHP\Db as Db;
 
@@ -10,15 +11,28 @@ use \ScpoPHP\Db as Db;
 
 if (!session_start()) $ret('服务端无法使用 session');
 
-if ($_FILES['doc']['error'] !== UPLOAD_ERR_OK) $ret('获取不到上传的文件');
-
-$cid = Db::insert([
-	'uid' => 1,
-], 'metas', true);
-if (!$cid) $ret('无法注册这个章节');
-
-$tempFile = $_FILES['doc']['tmp_name'];
-$targetFile = __DIR__ . '/../ebook/book_' . $cid;
-if (!move_uploaded_file($tempFile, $targetFile)) $ret('无法移动临时文件');
-
-$end(json_encode(['code' => 0, 'info' => $cid]));
+switch ($_POST['step']) {
+	case 'auth':
+		if (!isset($_SESSION['uid'])) $ret('你没登录');
+		$fid = uniqid('', true);
+		$_SESSION['fid'] = $fid;
+		$end(json_encode(['authed', $fid]));
+	case 'uploaded':
+		if (!isset($_SESSION['fid'])) $ret('不知道上传的是什么');
+		$fid = $_SESSION['fid'];
+		if (!is_dir(FILE_DIR)) $end('["uploadFailed"]');
+		if (!isset($_SESSION['uid'])) {
+			$ret('你没登录');
+		}
+		$uid = $_SESSION['uid'];
+		$cid = Db::insert([
+			'uid' => $uid,
+			'fid' => $fid,
+		], 'metas', true);
+		if (!$cid) {
+			$end('["registFailed"]');
+		}
+		$end(json_encode(['success', $cid]));
+	default:
+		$ret('当前进行到上传的第几步了？');
+}
--- background/main.js
+++ background/main.js
@@ -1,7 +1,51 @@
 /// <reference path="../public/dist/common.d.ts" />
 /// <reference path="../public/dist/mods.d.ts" />
 
+/**@type {string | null} */
+let fid = null;
+
 initCallbackHandler({
-	start: [],
+	start: ['upload', () => gid('status_check_input', 'input').click()],
+	authed: ['upload', ([fidGot]) => fid = fidGot],
+	uploadFailed: [noForm, ['上传失败', '请重新上传']],
+	registFailed: ['upload', ['无法注册此内容', '请重新上传']],
+	success: ['success', ([cid]) => gid('succ_url_a', 'a').href = `/ebook/?cid=${cid}`],
 });
+
+const pandoc = new mods.Pandoc(
+	fetch('/public/lib/pandoc.wasm'),
+	{ err: msg => wrong(Error(msg)) },
+);
+pandoc.init();
+
+const uploader = new mods.Resumable({
+	target: '/api/upload/trans.php',
+	// @ts-ignore
+	testTarget: '/api/upload/check.php',
+	chunkSize: 800 * 1024, // 800 KB
+	forceChunkSize: true,
+	simultaneousUploads: 10,
+	testChunks: true,
+});
+uploader.on('progress', () => gid('upload_progress', 'progress').value = uploader.progress() * 100);
+uploader.on('complete', () => gid('uploaded_callback_input', 'input').click());
+uploader.on('filesAdded', () => uploader.upload());
+
+setOnload(() => {
+	const docInput = gid('doc_input', 'input');
+	const uploadButton = gid('upload_button', 'button');
+	uploadButton.onclick = async () => {
+		if (docInput.files === null) wrong(Error('你没选文件'));
+		if (docInput.files.length > 1) wrong(Error('一次只能上传一个文件'));
+		uploader.cancel();
+		const { data, medias } = await pandoc.parse(
+			`-f docx -t json --mathjax --extract-media=${fid}/`,
+			await docInput.files[0].bytes(),
+			`${fid}/media`,
+		);
+		medias.push(new File([data], 'index.json'));
+		uploader.addFiles(medias);
+	};
+});
+
 
