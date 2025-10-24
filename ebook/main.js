52aeee8d0deb1d15768aa24160d599d854724507
E0SelmY4V <yabo1887415157@qq.com>
2025-10-24 15:24:24 +0800

    add: 能够拿到文章结构 json

--- ebook/main.js
+++ ebook/main.js
@@ -1,9 +1,25 @@
 /// <reference path="../public/common.ts" />
 
+const findedObjType = Type.Object({
+	id: Type.String(),
+	uid: Type.String(),
+	update_time: Type.String(),
+	fid: Type.String(),
+});
+
 initCallbackHandler({
 	start: [noForm],
-	finded: ['main'],
+	finded: ['main', render],
 }, {
-	finded: [Type.Object({ uid: Type.Number(), update_time: Type.String(), fid: Type.String() })],
+	finded: [findedObjType],
 });
+
+/**
+ * @param {Type.Static<typeof findedObjType>} finded
+ */
+async function render({ fid }) {
+	const res = await req(`/api/upload/files/${fid}/index.json`);
+	const content = await res.json();
+	console.log(content);
+}
 
