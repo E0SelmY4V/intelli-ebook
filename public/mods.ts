5c162f22b27820bc2639a4faaea718afcd1cabdc
E0SelmY4V <yabo1887415157@qq.com>
2025-12-20 07:42:48 +0800

    ref: 对 zod 的导入更简洁
    鉴于对 zod 的 tree shake 作用不大

--- public/mods.ts
+++ public/mods.ts
@@ -7,22 +7,10 @@ import * as zOri from 'zod';
 import * as PandocTypesOri from './lib/pandoc';
 import * as Mods from './mods';
 
-namespace zImported {
-	export import object = zOri.object;
-	export import tuple = zOri.tuple;
-	export import string = zOri.string;
-	export import number = zOri.number;
-	export import ZodTuple = zOri.ZodTuple;
-	export import ZodType = zOri.ZodType;
-	export type infer<T> = zOri.infer<T>;
-	export type output<T> = zOri.output<T>;
-	export type input<T> = zOri.input<T>;
-}
-
 declare global {
 	namespace globalThis {
 		export import mods = Mods;
-		export import z = zImported;
+		export import z = zOri;
 		export import Pandoc = Mods.PandocOri;
 		export import PandocTypes = PandocTypesOri;
 	}
@@ -31,7 +19,7 @@ export { default as Resumable } from 'resumablejs';
 export { default as PandocOri } from './lib/pandoc';
 
 globalThis.mods = Mods;
-globalThis.z = zImported;
+globalThis.z = zOri;
 globalThis.Pandoc = Mods.PandocOri;
 globalThis.PandocTypes = PandocTypesOri;
 
