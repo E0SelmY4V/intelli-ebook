cf046edf35a590a4eecdf9066ac3460adf1b0772
E0SelmY4V <yabo1887415157@qq.com>
2025-12-31 16:23:35 +0800

    add: 正确的滑动条

--- ebook/src/docproc.ts
+++ ebook/src/docproc.ts
@@ -112,7 +112,9 @@ export class Renderer {
 	 */
 	protected geleBody(this: this, ...[blocks, ele]: [Block[]] | [HTMLDivElement, HTMLDivElement]): HTMLDivElement {
 		const div = gele('div', {
-			className: ele ? 'show_body show_body_grouped' : 'show_body',
+			className: ele
+				? 'show_body show_body_grouped'
+				: 'show_body show_body_text',
 		});
 		this.hide(div);
 		if (Array.isArray(blocks)) {
