e4ad43f5804daa072c262a3f0aa7357dbecb97dd
E0SelmY4V <yabo1887415157@qq.com>
2026-02-07 00:57:59 +0800

    ref: 打包 mathjax

--- ebook/build.ts
+++ ebook/build.ts
@@ -1,5 +1,12 @@
 import { build } from 'esbuild';
+import * as fsp from 'node:fs/promises';
+import { join, dirname } from 'node:path';
 
+const mjDir = dirname(new URL(import.meta.resolve('mathjax')).pathname);
+const distDir = dirname(new URL(import.meta.resolve('./dist/main.js')).pathname);
+
+fsp.cp(join(mjDir, 'tex-chtml.js'), join(distDir, 'tex-chtml.js'));
+fsp.cp(join(mjDir, 'sre'), join(distDir, 'sre'), { recursive: true });
 build({
 	entryPoints: ['src/main.ts'],
 	bundle: true,
