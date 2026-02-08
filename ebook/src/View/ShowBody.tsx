03348753ebb2fc9b631b347794e075dcde8d0cc3
E0SelmY4V <yabo1887415157@qq.com>
2026-02-08 08:53:57 +0800

    add: 侧栏可以调整大小

--- ebook/src/View/ShowBody.tsx
+++ ebook/src/View/ShowBody.tsx
@@ -3,6 +3,7 @@ import { Fragment, JSX, useState } from 'react';
 import { Serialized } from '../storage';
 import Header from './Header';
 import ShowBodyText from './ShowBodyText';
+import { Resizable } from 're-resizable';
 
 export default function ShowBody({ serialized, hidden }: {
 	serialized: Serialized;
@@ -20,15 +21,32 @@ export default function ShowBody({ serialized, hidden }: {
 		{
 			content.map(([header, body], idx) => <ShowBody serialized={body} hidden={hidden || showing !== idx} key={header} />)
 		}
-		<div className={clsx('show_body', 'show_body_header_box')} hidden={hidden || !content.length}>
-			{content.map(([header], idx) => <Fragment key={header}>
-				<Header
-					header={header}
-					hidden={idx !== showing}
-					show={() => setShowing(showing === idx ? -1 : idx)}
-				/>
-				<hr />
-			</Fragment>)}
+		<div hidden={hidden || !content.length}>
+			<Resizable
+				defaultSize={{ width: '20vw', height: '100%' }}
+				enable={{
+					top: false,
+					right: true,
+					bottom: false,
+					left: false,
+					topRight: false,
+					bottomRight: false,
+					bottomLeft: false,
+					topLeft: false,
+				}}
+				handleClasses={{ right: 'show_body_handle' }}
+			>
+				<div className={clsx('show_body', 'show_body_header_box')}>
+					{content.map(([header], idx) => <Fragment key={header}>
+						<Header
+							header={header}
+							hidden={idx !== showing}
+							show={() => setShowing(showing === idx ? -1 : idx)}
+						/>
+						<hr />
+					</Fragment>)}
+				</div>
+			</Resizable>
 		</div>
 	</>;
 }
